b32bfc5e63208daf2a39f6bd10b0ebc47ec066d8
Sam Hecht <s013401@students.lmsd.org>
2020-02-18 12:58:49 -0500

    Initial Commit

---
+++ GroupUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupUp
+//
+//  Created by Samuel Hecht (student LM) on 2/18/20.
+//  Copyright © 2020 Samuel Hecht (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
