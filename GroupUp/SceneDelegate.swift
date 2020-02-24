1bcbbecec7a68499793c54bee639079c8d07ca7e
Seth <s013411@students.lmsd.org>
2020-02-24 13:35:10 -0500

    eqrghjk;/.

--- GroupUp/SceneDelegate.swift
+++ GroupUp/SceneDelegate.swift
@@ -25,7 +25,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = MapViewController()
+        let nc = UINavigationController(rootViewController: MapViewController())
+        window?.rootViewController = nc
         window?.makeKeyAndVisible()
         
         
