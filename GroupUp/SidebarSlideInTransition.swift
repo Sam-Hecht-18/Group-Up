d81bf992f28254d7fca7c52d717b63f1a49eed24
Sam Hecht <s013401@students.lmsd.org>
2020-02-24 13:01:42 -0500

    Added a couple view controllers for Eli

---
+++ GroupUp/SidebarSlideInTransition.swift
@@ -0,0 +1,9 @@
+//
+//  SidebarSlideInTransition.swift
+//  GroupUp
+//
+//  Created by Samuel Hecht (student LM) on 2/24/20.
+//  Copyright © 2020 Samuel Hecht (student LM). All rights reserved.
+//
+
+import Foundation
