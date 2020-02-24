f226d3862ef62644f89116cf2406654d4639be8d
Vik Miller <vikazmilla@gmail.com>
2020-02-24 13:18:11 -0500

    Start up screen VC hopefully fixed

--- GroupUp/StartUpScreenViewController.swift
+++ GroupUp/StartUpScreenViewController.swift
@@ -1,8 +1,30 @@
 //
 //  StartUpScreenViewController.swift
-//  
+//  GroupUp
 //
-//  Created by Seth Richards (student LM) on 2/24/20.
+//  Created by Vikas Miller (student LM) on 2/24/20.
+//  Copyright © 2020 Samuel Hecht (student LM). All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class StartUpScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
