9d9eb00c61e1c8f814ae6d6ac083519889698b10
Seth <s013411@students.lmsd.org>
2020-03-04 12:18:33 -0500

    made Event Creator class

---
+++ GroupUp/EventCreator.swift
@@ -0,0 +1,9 @@
+//
+//  EventCreator.swift
+//  GroupUp
+//
+//  Created by Seth Richards (student LM) on 3/4/20.
+//  Copyright © 2020 Samuel Hecht (student LM). All rights reserved.
+//
+
+import Foundation
