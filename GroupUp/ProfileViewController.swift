0d663d20c80cb7b9cdf74ea1c583383a0767df3b
Sam Hecht <s013401@students.lmsd.org>
2020-03-04 18:11:47 -0500

    Eli's stuff but without trash errors

--- GroupUp/ProfileViewController.swift
+++ GroupUp/ProfileViewController.swift
@@ -9,49 +9,49 @@
 import UIKit
 
 class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-                
-//            var imagePicker : UIImagePickerController?
-//
+    //            var imagePicker : UIImagePickerController?
+    //
     
     @IBOutlet weak var profilePic: UIButton!
     @IBAction func changeProfilePic(_ sender: UIButton) {
         print ("mate")
     }
     
-
+    
     override func viewDidLoad() {
         print("yessirrr")
-//        imagePicker = UIImagePickerController()
-//            imagePicker?.allowsEditing = true
-//            imagePicker?.sourceType = .photoLibrary
-//            imagePicker?.delegate = self
-
+        //        imagePicker = UIImagePickerController()
+        //            imagePicker?.allowsEditing = true
+        //            imagePicker?.sourceType = .photoLibrary
+        //            imagePicker?.delegate = self
+        
         profilePic.layer.borderWidth = 1
         profilePic.layer.masksToBounds = false
-            profilePic.layer.borderColor = UIColor.black.cgColor
-            profilePic.layer.cornerRadius = profilePic.frame.height/2
-            profilePic.clipsToBounds = true
+        profilePic.layer.borderColor = UIColor.black.cgColor
+        profilePic.layer.cornerRadius = profilePic.frame.height/2
+        profilePic.clipsToBounds = true
+         
         
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
     }
-//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-//           imagePicker?.dismiss(animated: true, completion: nil)
-//       }
-//       func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//           if let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
-//               profilePic.image = pickedImage
-////               uploadProfilePicture(pickedImage) {url in}
-//           }
-//           imagePicker?.dismiss(animated: true, completion: nil)
-//       }
-////       func uploadProfilePicture(_ image: UIImage, _ completion: @escaping((_ url:URL?) -> ())){
-////           guard let uid = Auth.auth().currentUser?.uid else {return}
-////           let storage = Storage.storage().reference().child("user /\(uid)")
-////           guard let image = imageView?.image, let imageData = image.jpegData(compressionQuality: 0.75) else {return}
-////           storage.putData(imageData, metadata: StorageMetadata()) { (metaData, error) in }
-////
-////       }
-//}
+    //    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    //           imagePicker?.dismiss(animated: true, completion: nil)
+    //       }
+    //       func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    //           if let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+    //               profilePic.image = pickedImage
+    ////               uploadProfilePicture(pickedImage) {url in}
+    //           }
+    //           imagePicker?.dismiss(animated: true, completion: nil)
+    //       }
+    ////       func uploadProfilePicture(_ image: UIImage, _ completion: @escaping((_ url:URL?) -> ())){
+    ////           guard let uid = Auth.auth().currentUser?.uid else {return}
+    ////           let storage = Storage.storage().reference().child("user /\(uid)")
+    ////           guard let image = imageView?.image, let imageData = image.jpegData(compressionQuality: 0.75) else {return}
+    ////           storage.putData(imageData, metadata: StorageMetadata()) { (metaData, error) in }
+    ////
+    ////       }
+    //}
 }
