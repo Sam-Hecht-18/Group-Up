5df815e67713dfd75c2d24c7aabdd6091f8112cb
Seth <s013411@students.lmsd.org>
2020-03-04 12:19:51 -0500

    event creator code

--- GroupUp/EventCreator.swift
+++ GroupUp/EventCreator.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import UIKit
+
+class EventCreator: UIViewController{
+    let label = UILabel()
+
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.black
+
+    
+        label.textColor = UIColor.white
+        label.text = "New Event"
+        label.frame = CGRect(x: 30, y: 30, width: 250, height: 30)
+        label.font = UIFont(name: "Arial", size: 35)
+
+        self.view.addSubview(label)
+        
+    }
+    
+    
+}
