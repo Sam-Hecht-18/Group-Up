9aed4a34607585e0a7f69a22f9358a529d58f9a9
Sam Hecht <s013401@students.lmsd.org>
2020-03-05 18:16:28 -0500

    Put NAV on table view

--- GroupUp/NavigationController.swift
+++ GroupUp/NavigationController.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+import UIKit
+class NavigationController: UINavigationController, UINavigationControllerDelegate{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
